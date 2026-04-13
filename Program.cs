26cd7c5196886838b53eec858c63b92f8ef53eac
Zukhanyise <zukhanyisevitshima@gmail.com>
2026-04-13 10:19:30 +0200

    Added main source code file Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CyberSecurityBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            chatbot bot = new chatbot();
+            bot.Start();
+        }
+    }
+}
